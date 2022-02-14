50dac20130ceb20310e54a4c227be0f291aa3961
Soheil Ataie <75911333+SoheilAtaie@users.noreply.github.com>
2022-02-14 10:34:33 -0500

    Create BaseCharacter.h

---
+++ BaseCharacter.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "raylib.h"
+
+#ifndef BASE_CHARACTER_H
+#define BASE_CHARACTER_H
+
+class BaseCharacter
+{
+public:
+	BaseCharacter();
+	Vector2 getWorldPos() { return worldPos; }
+	void undoMovement();
+	Rectangle getCollisonRec();
+	virtual void tick(float deltaTime);
+	virtual Vector2 getScreenPos() = 0;
+	bool getAlive() { return alive; }
+	void setAlive(bool isAlive) { alive = isAlive;  }
+protected:
+	Texture2D texture{ LoadTexture("characters/knight_idle_spritesheet.png") };
+	Texture2D idle{ LoadTexture("characters/knight_idle_spritesheet.png") };
+	Texture2D run{ LoadTexture("characters/knight_run_spritesheet.png") };
+	Vector2 worldPos{};
+	Vector2 worldPosLastFrame{};
+	// Character variables
+	float width{};
+	float height{};
+
+	// 1 : facing right, -1 : facing left
+	float rightLeft{ 1.f };
+
+	// animation variables
+	float runningTime{};
+	int frame{};
+	const int maxFrame{ 6 };
+	const float updateTime{ 1.f / 12.f };
+	float speed{ 4.f };
+
+	float scale{ 4.0f };
+	Vector2 velocity{};
+private:
+	bool alive{ true };
+};
+
+#endif // !BASE_CHARACTER_H
+
